3b8d33d6e0277c9d6339874c12481ad661c65ece
Sara Mae <34497797+saramaebee@users.noreply.github.com>
2021-08-21 01:43:01 -0400

    yaml

--- server.js
+++ server.js
@@ -21,8 +21,16 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  res.json({
+	  ipaddress: req.ip,
+	  language: req.headers['accept-language'],
+	  software: req.headers['user-agent'],
+  });
+});
+app.get(('/api/raw'), function (req, res) {
+	// console.dir(req.headers);
+	res.send(req.headers);	
 });
 
 
