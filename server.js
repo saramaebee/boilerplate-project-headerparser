72242aaa273c588704ca0db365b0b9b939eca346
Sara Mae <34497797+saramaebee@users.noreply.github.com>
2021-08-21 02:37:54 -0400

    try logging ig?

--- server.js
+++ server.js
@@ -9,33 +9,36 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({
+	optionsSuccessStatus: 200
+})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/whoami", function (req, res) {
-  res.json({
-	  ipaddress: req.headers['X-Forwarded-For'],
-	  language: req.headers['accept-language'],
-	  software: req.headers['user-agent'],
-  });
+	console.log(req.headers['X-Forwarded-For'])
+	res.json({
+		ipaddress: req.headers['X-Forwarded-For'],
+		language: req.headers['accept-language'],
+		software: req.headers['user-agent'],
+	});
 });
 app.get(('/api/raw'), function (req, res) {
 	// console.dir(req.headers);
-	res.send(req.headers);	
+	res.send(req.headers);
 });
 
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log('Your app is listening on port ' + listener.address().port);
 });
